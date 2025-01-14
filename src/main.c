68b82a7eb2614effd2c32d84dea6c9d12497e22d
SilverThirth <silverthirth@gmail.com>
2025-01-14 08:42:00 -0500

    Add a few test comments

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include "local_lib.h"
 
+/*!
+ * Entry point for program
+ */
 
 int main()
 {
-    char name[10];
+    char name[10]; /// Test
 
-    for(;;)
+    for(;;) /*! - Endless loop */
     {
         get_input(name, CHAR_NAME_LIMIT);
         printf("\n%s\n", name);
